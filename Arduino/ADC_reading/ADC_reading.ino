095dc15e551a983b4a7cdd46f93c46619e380ed7
Yuliya Cherapukha <ycherapukha@908devices.com>
2017-11-11 19:33:54 -0500

    ADC reading

---
+++ Arduino/ADC_reading/ADC_reading.ino
@@ -0,0 +1,15 @@
+int sensorPin = A0;    // select the input pin for the potentiometer
+int sensorValue = 0;  // variable to store the value coming from the sensor
+
+
+void setup() {
+  // initialize serial communications at 9600 bps:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // read the value from the sensor:
+  sensorValue = analogRead(sensorPin);
+  Serial.println(sensorValue);
+  delay(500);
+}
